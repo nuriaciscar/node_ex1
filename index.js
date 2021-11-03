338cd393c25321b22ff600a62063a578cd2a2318
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-03 19:36:54 +0100

    Printed operations on server

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const http = require("http");
 
 const server = http.createServer();
 
-const { add, substraction, multiply, divide } = require("./operations");
+const results = require("./calculator");
 
 const port = process.env.SERVER_URL || 5000;
-
-const html = `<h1>Resultados</h1><ul><li>6 + 3 = 9</li><li>6 - 3 = 3</li><li>6 * 3 = 18</li><li>6 / 3 = 2</li></ul>`;
 
 server.listen(port);
 
@@ -15,6 +13,6 @@ server.on("request", (request, response) => {
   // const queryParam = port.parse(request.port, true).query;
   // console.log(queryParam);
   response.setHeader("Content-type", "text/html");
-  response.write(JSON.stringify(html));
+  response.write(results(1, 2));
   response.end();
 });
